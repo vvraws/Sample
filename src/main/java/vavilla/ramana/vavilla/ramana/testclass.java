facd52825a1b81f24edc94b99510a3d7e2b52d4c
vavilla venkataramana <vvraws@gmail.com>
2017-12-25 22:20:21 -0500

    this is first project

---
+++ src/main/java/vavilla/ramana/vavilla/ramana/testclass.java
@@ -0,0 +1,12 @@
+package vavilla.ramana.vavilla.ramana;
+
+public class testclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this is my first java program");
+
+	}
+
+}
